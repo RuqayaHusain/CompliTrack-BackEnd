90fdb783bc0bdf606d63d3fed5f5c2f9dac215f7
Ruqaya Husain <ruqayanader22@gmail.com>
2025-12-19 17:02:46 +0300

    added business serializer

---
+++ serializers/business.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from datetime import datetime
+from .user import UserSchema
+from models.business import IndustryEnum
+from models.license import LicenseStatusEnum
+
+class BusinessCreate(BaseModel):
+    """Schema for creating a new business"""
+    name: str = Field(..., min_length=1, max_length=255, description="Title of the Business")
+    description: Optional[str] = Field(None, description="Description of the Business")
+    cr_number: str = Field(..., min_length=1, max_length=50, description="CR Number of the Business")
+    industry: IndustryEnum
+    image_url = Optional[str] = Field(None, description="Business Logo")
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "My First Business!",
+                "description": "A sample business description",
+                "cr_number": "CR234",
+                "industry": "RETAIL",
+                "image_url": "https://img.freepik.com/free-vector/business-logo-template-minimal-branding-design-vector_53876-136229.jpg?semt=ais_hybrid&w=740&q=80"
+            }
+        }
+
+class BusinessUpdate(BaseModel):
+    """Schema for updating a business (all fields optional)"""
+    name: Optional[str] = Field(None, min_length=1, max_length=255)
+    description: Optional[str] = Field(None)
+    cr_number: Optional[str] = Field(None, min_length=1, max_length=50)
+    industry: Optional[IndustryEnum] = None
+    image_url = Optional[str] = Field(None)
+    
+
+class LicenseSchema(BaseModel):
+    """Basic License schema for nested display"""
+    id: int
+    name: str
+    description: str
+    issue_date: datetime
+    expiry_date: datetime
+    status: LicenseStatusEnum
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+class BusinessSchema(BaseModel):
+    """Schema for returning business data"""
+    id: int
+    name: str
+    description: Optional[str]
+    cr_number: str
+    industry: IndustryEnum
+    image_url: Optional[str]
+    created_at: datetime
+    user: UserSchema
+    licenses: List[LicenseSchema] = []
+
+    class Config:
+        orm_mode = True
